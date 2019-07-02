9a071d0ec12123afa073a5af1cfbf08a2d99b0bd
Ryo1916 <telemon1916@gmail.com>
2019-07-02 12:07:11 +0900

    Set sendgrid configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "app_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'https://music-review-production.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'music-review-production.herokuapp.com',
+    authentication: :plain,
+    enable_starttls_auto: true,
+    user_name: 'apikey',
+    password: Rails.application.credentials.sendgrid_api_key
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
